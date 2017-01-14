bbc4aa762ee4e54c98b977f1579b9d9376812881
julianit <yulian_iliev@yahoo.com>
2017-01-14 06:54:57 -0800

    Convert class to type

--- TypeConverter.cpp
+++ TypeConverter.cpp
@@ -27,6 +27,13 @@ int main(void)
 	const char *nptr = "123";
 	char *endptr = "123";
 	int base = 10;
+	
+	//Here is the interesting part 
+	//1. Calls ctr
+	//2. Calls 'operator long long' because of the lvalue
 	long long ll = Converter(nptr, &endptr, base);
+	
+	//1. Calls ctr
+	//2. Calls 'operator unsigned long' because of the lvalue
 	unsigned long l = Converter(nptr, &endptr, base);
 }
